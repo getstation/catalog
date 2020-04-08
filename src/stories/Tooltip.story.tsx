6f94209003bb20d05d78a26824d85bcb2644e430
vision-4 <romain.daniel@epitech.eu>
2020-04-08 17:21:00 +0200

    feat(bit.dev): add Tooltip component to bit.dev catalog

---
+++ src/stories/Tooltip.story.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import color from 'color';
+import {storiesOf} from '@storybook/react';
+import {number, select, text} from '@storybook/addon-knobs';
+
+import Colors from "@components/Colors";
+import Tooltip, {TooltipPositions} from '@components/Tooltip';
+import Icon, {Icons} from "@components/Icon";
+
+import {createUseStyles} from "react-jss";
+
+const useStyle = createUseStyles({
+  icon: {
+    padding: 13,
+    margin: 8,
+    zIndex: 1,
+    backgroundColor: color(Colors.lightPrimaryBackgroundColor)
+      .fade(0.6)
+      .string(),
+    borderRadius: 22,
+    transition: 'background-color 0.2s',
+    '&:hover': {
+      backgroundColor: color(Colors.lightPrimaryBackgroundColor)
+        .fade(0.8)
+        .string(),
+      '&:active': {
+        backgroundColor: color(Colors.lightPrimaryBackgroundColor)
+          .fade(0.9)
+          .string()
+      }
+    }
+  },
+});
+
+storiesOf('Components|Tooltip', module)
+  .addParameters({options: {selectedPanel: 'storybook/Tooltip'}})
+  .add('Basic', () =>
+    <Tooltip text={text('text', 'Station Hub')} styles={{duration: '0 !important', exitDuration: '0 !important', delay: '0 !important'}}>
+      <Icon icon={Icons.STATION} className={useStyle().icon} size={19}/>
+    </Tooltip>)
+  .add('with custom style', () => (
+    <Tooltip
+      text={text('text', 'Custom Hub', 'tooltip')}
+      styles={{
+        space: number('space', 42, { range: true, min: 0, max: 200, step: 1 }, 'tooltip'),
+        duration: number('duration (ms)', 350, { range: true, min: 0, max: 2000, step: 10 }, 'tooltip'),
+        exitDuration: number('exitDuration (ms)', 600, { range: true, min: 0, max: 2000, step: 10 }, 'tooltip'),
+        delay: number('delay (ms)', 0, { range: true, min: 0, max: 1500, step: 10 }, 'tooltip'),
+        position: select('position', Object.values(TooltipPositions) as TooltipPositions[], TooltipPositions.LEFT, 'tooltip'),
+        margin: text('margin', '20px', 'tooltip'),
+        container: {
+          color: text('color', 'blue', 'box'),
+          radius: number('radius', 20, { range: true, min: 0, max: 30, step: 1 }, 'box'),
+          padding: text('padding', '20px', 'box')
+        },
+        text: {
+          color: text('color', 'red', 'text'),
+          size: number('size', 22, { range: true, min: 0, max: 30, step: 1 }, 'text'),
+        }
+      }}
+    >
+      <Icon icon={Icons.COG} size={100}/>
+    </Tooltip>));
