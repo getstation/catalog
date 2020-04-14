ffe3b0ae028d1d0f496ef6d0b8915365b2d2c76c
vision4 <romain.daniel@epitech.eu>
2020-04-14 15:47:04 +0200

    feat(teamhub): add resources related icons

---
+++ src/components/Icon/svg/PlusSvg.tsx
@@ -0,0 +1,7 @@
+/* tslint:disable:max-line-length */
+;
+import * as React from 'react';
+
+const PlusSvg = (props: React.SVGProps<SVGSVGElement>) => <svg width={12} height={12} viewBox="0 0 12 12" {...props}><path fillRule="evenodd" d="M5,5 L5,1 C5,0.4477 5.4477,0 6,0 C6.5523,0 7,0.4477 7,1 L7,5 L11,5 C11.5523,5 12,5.4477 12,6 C12,6.5523 11.5523,7 11,7 L7,7 L7,11 C7,11.5523 6.5523,12 6,12 C5.4477,12 5,11.5523 5,11 L5,7 L1,7 C0.4477,7 0,6.5523 0,6 C0,5.4477 0.4477,5 1,5 L5,5 Z" /></svg>;
+
+export default PlusSvg;
