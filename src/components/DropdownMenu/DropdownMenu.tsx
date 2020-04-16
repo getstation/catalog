b7250bd86ae6cf8bfd2cb8aad8ca0cf11f542abd
vision-4 <romain.daniel@epitech.eu>
2020-04-16 14:49:40 +0200

    feat(catalog): add DropdownMenu component w/ stories

---
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import color from 'color';
+import classNames from 'classnames';
+import {createUseStyles} from 'react-jss';
+import Image from "../Image";
+import Colors from '../Colors';
+import Icon, {Icons} from '../Icon';
+import {DropdownMenuStyles, DropdownMenuProps} from './index';
+
+const useStyles = createUseStyles({
+  root: {
+    position: 'relative',
+    margin: (styles: DropdownMenuStyles) => styles?.margin || 0,
+    padding: (styles: DropdownMenuStyles) => styles?.padding || 0,
+    backgroundColor: 'transparent',
+    border: 'none',
+    outline: 'none',
+    '&:focus $items': {
+      display: 'flex',
+      opacity: 1,
+      transform: 'scale(1)',
+    }
+  },
+  items: {
+    opacity: 0,
+    flexDirection: 'column',
+    position: 'absolute',
+    overflow: 'hidden',
+    margin: 0,
+    top: (styles: DropdownMenuStyles) => `calc(100% + ${styles?.space || 2}px)`,
+    left: 0,
+    padding: (styles: DropdownMenuStyles) => styles?.items?.padding || '5px 0',
+    backgroundColor: Colors.lightSecondaryBackgroundColor,
+    border: ['solid', 0.5, '#cfd6db'],
+    borderRadius: 6,
+    transform: 'scale(0)',
+    transformOrigin: [0, 0],
+    transitionDuration: (styles: DropdownMenuStyles) => styles?.duration || '100ms',
+  },
+  item: {
+    height: 25,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    outline: 'none',
+    fontSize: 14,
+    letterSpacing: 0,
+    whiteSpace: 'nowrap',
+    padding: [0, 18],
+    color: Colors.lightPrimaryTextColor,
+    '&:hover': {
+      backgroundColor: Colors.lightTertiaryBackgroundColor
+    }
+  },
+  icon: {
+    minWidth: 12,
+    padding: '6px 14px 6px 0',
+    '& svg': {
+      position: 'relative',
+      top: -2,
+    }
+  },
+  image: {
+    padding: 5,
+  },
+  selectIcon: {
+    height: 25,
+    width: 20,
+    '& svg': {
+      boxSizing: 'border-box',
+      padding: 5,
+    }
+  },
+  unchecked: {
+    height: 25,
+    width: 20,
+  }
+});
+
+const DropdownMenu = ({ children,
+                        items,
+                        styles,
+                        checkmark = false,
+                        closeOnSelect = false,
+                        onSelect,
+                        className,
+                        itemClassName,
+                        listClassName }: DropdownMenuProps) => {
+  const classes = useStyles(styles);
+  return (
+    <button className={classNames(classes.root, className)}>
+      {children?.trigger || children}
+      <ul className={classNames(classes.items, listClassName)}>{items ? items.map(item =>
+        <li key={item.text} className={classNames(classes.item, itemClassName)} onClick={_ => !(onSelect) || onSelect(item)} tabIndex={closeOnSelect ? 1 : undefined}>
+          {checkmark && (item.selected
+            ? <Icon icon={Icons.CHECKMARK} size={12} color={Colors.lightPrimaryTextColor} className={classes.selectIcon}/>
+            : <div className={classes.unchecked}/>
+          )}
+          {item.icon && <Icon className={classes.icon} icon={item.icon} color={color(Colors.lightPrimaryTextColor).fade(0.4).string()} size={12}/>}
+          {item.image && <Image className={classes.image} image={item.image} size={20}/>}
+          <span>{item.text}</span>
+        </li>
+      ) : children?.items}</ul>
+    </button>
+  );
+};
+
+export default DropdownMenu;
---
+++ src/components/DropdownMenu/index.ts
@@ -0,0 +1,149 @@
+import {ReactChild} from "react";
+import DropdownMenu from './DropdownMenu';
+import {Icons} from "../Icon";
+import {Images} from "../../assets/images";
+
+export type DropdownMenuProps = {
+  /**
+   * @name children
+   * @type DropdownMenuSlots | any
+   * @desc Can be a simple trigger JSX Element OR slotted children object (see `DropdownMenuSlots`)
+   */
+  children: DropdownMenuSlots | any;
+  /**
+   * @name items
+   * @type DropdownMenuOption[]
+   * @desc array of text and optional image/icon used to generate dropdown menu items (possibility to set selected value(s) if using `checkmark` option)
+   */
+  items?: DropdownMenuOption[];
+  /**
+   * @name styles
+   * @type DropdownMenuStyles
+   * @desc Styling options
+   */
+  styles?: DropdownMenuStyles;
+  /**
+   * @name checkmark
+   * @type boolean
+   * @default false
+   * @desc is checkmark icon present on `selected = true` items
+   */
+  checkmark?: boolean;
+  /**
+   * @name closeOnSelect
+   * @type boolean
+   * @default false
+   * @desc if true, close the menu box on item click
+   */
+  closeOnSelect?: boolean;
+  /**
+   * @name onSelect
+   * @type function
+   * @param e: DropdownMenuOption
+   * @desc called on item click with item object passed to param
+   */
+  onSelect?: (e: DropdownMenuOption) => void;
+  /**
+   * @name className
+   * @type string
+   * @desc root container css class name
+   */
+  className?: string;
+  /**
+   * @name itemClassName
+   * @type string
+   * @desc item css class name
+   */
+  itemClassName?: string;
+  /**
+   * @name listClassName
+   * @type string
+   * @desc content box css class name
+   */
+  listClassName?: string;
+};
+
+
+export type DropdownMenuSlots = {
+  trigger?: ReactChild;
+  items?: ReactChild[];
+};
+
+export type DropdownMenuOption = {
+  icon?: Icons;
+  image?: Images | string;
+  text: string;
+  selected?: boolean;
+};
+
+export type DropdownMenuStyles = {
+  /**
+   * @name duration
+   * @default 100
+   * @desc entry animation duration (in milliseconds)
+   */
+  duration?: number | string;
+  /**
+   * @name space
+   * @default 2
+   * @desc space between trigger and content box
+   */
+  space?: number;
+  /**
+   * @name margin
+   * @default 0
+   * @desc margin of the root container, will not trigger the DropdownMenu
+   */
+  margin?: (string | number)[] | string | number;
+  /**
+   * @name padding
+   * @default 0
+   * @desc padding of the root container
+   */
+  padding?: (string | number)[] | string | number;
+  /**
+   * @name items
+   * @desc styles of content box
+   */
+  items?: {
+    /**
+     * @name color
+     * @default Colors.lightSecondaryBackgroundColor
+     * @desc background-color of content box
+     */
+    color?: string;
+    /**
+     * @name radius
+     * @default 6
+     * @desc border radius of content box
+     */
+    radius?: (string | number)[] | string | number;
+    /**
+     * @name padding
+     * @default '5px 0'
+     * @desc padding of content box
+     */
+    padding?: string | number;
+  },
+  /**
+   * @name item
+   * @desc styles of each list item
+   */
+  item?: {
+    /**
+     * @name color
+     * @default Colors.lightPrimaryTextColor
+     * @desc color of text
+     */
+    color?: string;
+    /**
+     * @name size
+     * @default 14
+     * @desc font-size of text
+     */
+    size?: number | string;
+  },
+};
+
+export default DropdownMenu;
+export * from './DropdownMenu';
