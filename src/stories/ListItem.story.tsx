6826768e2cdad113b2a1723b10cb10512f0849ee
vision-4 <romain.daniel@epitech.eu>
2020-04-21 17:41:15 +0200

    feat(list-item): add children and isTeamhub props

--- src/stories/ListItem.story.tsx
+++ src/stories/ListItem.story.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import {createUseStyles} from "react-jss";
 import { storiesOf } from '@storybook/react';
-import { text, select } from '@storybook/addon-knobs';
+import {text, select, boolean} from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 import ListItem from '@components/ListItem';
+import Icon, {Icons} from "@components/Icon";
+import Colors from "@components/Colors";
+import Tooltip from "@components/Tooltip";
 
 const types = {
   tab: 'TAB',
   none: undefined,
 };
+
+const useStyle = createUseStyles({
+  icon: {
+    '&:hover': {
+      backgroundColor: Colors.lightTertiaryBackgroundColor,
+    },
+    alignSelf: 'center',
+    height: 12,
+    width: 12,
+    padding: 4,
+    borderRadius: 20,
+    '&> svg': {
+      display: 'block',
+      width: 12,
+      height: 12,
+    }
+  }
+})
 
 storiesOf('Components|ListItem', module)
   .addParameters({ options: { selectedPanel: 'storybook/ListItem' } })
@@ -71,6 +93,33 @@ storiesOf('Components|ListItem', module)
         url={'https://google.com/'}
         onClick={e => action('onListItemClicked')(e)}
       />
+    ),
+    {
+      centered: { disable: true },
+    },
+  )
+  .add(
+    'with children (teamhub)',
+    () => (
+      <ListItem
+        favIconUrl={text('favIconUrl', 'https://zeplin.io/img/favicon/228x228.png')}
+        title={text('title', 'Slack URLs Integrations')}
+        subtitle={text('appName', 'Notion')}
+        isTeamhub={boolean('isTeamhub', true)}
+        type={select('type', types, types.tab)}
+        url={'https://google.com/'}
+        onClick={e => action('onListItemClicked')(e)}
+      >
+        <Tooltip text={'Copy link'} styles={{space: -12, margin: '0 7px 0 0', text: {size: 10}}}>
+          <Icon icon={Icons.LINK} color={Colors.lightPrimaryTextColor} className={useStyle().icon} onClick={action('Copy link clicked')}/>
+        </Tooltip>
+        <Tooltip text={'Edit resource'} styles={{space: -12, margin: '0 7px 0 0', text: {size: 10}}}>
+          <Icon icon={Icons.EDITOR} color={Colors.lightPrimaryTextColor} className={useStyle().icon} onClick={action('Edit resource clicked')}/>
+        </Tooltip>
+        <Tooltip text={'Delete resource'} styles={{space: -12, margin: '0 16px 0 0', text: {size: 10}}}>
+          <Icon icon={Icons.TRASH} color={Colors.lightPrimaryTextColor} className={useStyle().icon} onClick={action('Delete resource clicked')}/>
+        </Tooltip>
+      </ListItem>
     ),
     {
       centered: { disable: true },
