92f5a1b466d2bb53e36518785b1c8671a6fc862d
vision-4 <romain.daniel@epitech.eu>
2020-04-23 19:23:35 +0200

    feat(ui): add Shortcut component w/ stories

---
+++ src/stories/Shortcut.story.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import Shortcut, {Key} from "@components/Shortcut";
+import {number} from "@storybook/addon-knobs";
+import {createUseStyles} from "react-jss";
+
+const useStyles = createUseStyles({
+  shortcut: {
+    background: 'rgb(31, 32, 35)',
+    padding: 25,
+    borderRadius: 20
+  },
+  key: {
+    color: 'rgb(215, 216, 219)',
+    backgroundColor: 'rgb(80, 80, 84)',
+    border: 'none'
+  }
+});
+
+storiesOf('Components|Shortcut', module)
+	.addParameters({ options: { selectedPanel: 'storybook/Shortcut' } })
+  .add('Basic', () => (<Shortcut keys={[Key.COMMAND, 'E']}/>))
+	.add('Examples', () => (
+	  <div>
+	  Key symbol: <Shortcut keys={Key.COMMAND} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} />
+	  Character: <Shortcut keys={'E'} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} />
+	  Multiple letters key: <Shortcut keys={Key.CTRL} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} />
+	  Multiple key shortcut: <Shortcut keys={[Key.COMMAND, Key.OPTION, 'i']} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} />
+	  Impossible shortcut: <Shortcut keys={[Key.COMMAND, Key.OPTION, Key.CONTROL, Key.SHIFT, '❤️', Key.SPACEBAR, 'poney']} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} />
+	  Keyboard arrows: <Shortcut keys={[Key.UP, Key.DOWN, Key.LEFT, Key.RIGHT]} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} />
+    </div>
+	))
+  .add('Custom', () => {
+    const classes = useStyles();
+    return (<Shortcut keys={[Key.COMMAND, 'K']} size={number('size', 25, { range: true, min: 10, max: 800, step: 1 })} className={classes.shortcut} keyClassName={classes.key} />);
+  });
