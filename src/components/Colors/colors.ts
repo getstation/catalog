45c9307e251f14d6caa8814d1aef678241e74776
vision4 <romain.daniel@epitech.eu>
2020-06-03 17:00:31 +0200

    Update color

--- src/components/Colors/colors.ts
+++ src/components/Colors/colors.ts
@@ -1,24 +1,26 @@
-export const blue = "#3070cd";
-export const green = "#76cca5";
-export const lightBlue = "#66c6ed";
-export const lightRed = "#ff8686";
-export const lightErrorColor = "#e75858";
-export const lightErrorVariantColor = "#ff8686";
-export const lightPlaceholderTextColor = "#a0aeb8";
-export const lightPrimaryAccentColor = "#1f5bb2";
-export const lightPrimaryBackgroundColor = "#ffffff";
-export const lightPrimaryBorderColor = "#598dd7";
-export const lightPrimaryColor = "#3070cd";
-export const lightPrimaryFocusBackgroundColor = "#e0f4fb";
-export const lightPrimaryHoverBackgroundColor = "#eff9fd";
-export const lightPrimaryLinkColor = "#3070cd";
-export const lightPrimaryTextColor = "#3c505d";
-export const lightSecondaryAccentColor = "#3fafdd";
-export const lightSecondaryBackgroundColor = "#f6f9fb";
-export const lightSecondaryBorderColor = "#cfd6db";
-export const lightSecondaryColor = "#66c6ed";
-export const lightSecondaryHoverBackgroundColor = "#e7ecf0";
-export const lightSecondaryTextColor = "#a0aeb8";
-export const red = "#e75858";
-export const white = "#ffffff";
-export const yellow = "#efc657";
+module.exports = {
+  blue: "#3070cd",
+  green: "#76cca5",
+  lightBlue: "#66c6ed",
+  lightRed: "#ff8686",
+  lightErrorColor: "#e75858",
+  lightErrorVariantColor: "#ff8686",
+  lightPlaceholderTextColor: "#a0aeb8",
+  lightPrimaryAccentColor: "#1f5bb2",
+  lightPrimaryBackgroundColor: "#ffffff",
+  lightPrimaryBorderColor: "#598dd7",
+  lightPrimaryColor: "#3070cd",
+  lightPrimaryFocusBackgroundColor: "#e0f4fb",
+  lightPrimaryHoverBackgroundColor: "#eff9fd",
+  lightPrimaryLinkColor: "#3070cd",
+  lightPrimaryTextColor: "#3c505d",
+  lightSecondaryAccentColor: "#3fafdd",
+  lightSecondaryBackgroundColor: "#f6f9fb",
+  lightSecondaryBorderColor: "#cfd6db",
+  lightSecondaryColor: "#66c6ed",
+  lightSecondaryHoverBackgroundColor: "#e7ecf0",
+  lightSecondaryTextColor: "#a0aeb8",
+  red: "#e75858",
+  white: "#ffffff",
+  yellow: "#efc657",
+};
