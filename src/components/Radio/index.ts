39f03f1baf829e17661e8617600a465c81e1efa1
vision-4 <romain.daniel@epitech.eu>
2020-08-17 13:49:33 +0200

    feat(bit.dev): Radio UI component

---
+++ src/components/Radio/index.ts
@@ -0,0 +1,4 @@
+import Radio from './Radio';
+
+export * from './Radio';
+export default Radio;
---
+++ src/stories/Radio.story.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Radio, { Choice } from '@components/Radio/Radio';
+
+const choices: Choice[] = [
+  {
+    id: 'Yes',
+    text: 'Use Station in the New Tab',
+    value: true
+  },
+  {
+    id: 'No',
+    text: 'Show the default browser page',
+    value: false
+  },
+];
+
+storiesOf('Components|Radio', module)
+  .addParameters({ options: { selectedPanel: 'storybook/Radio' } })
+  .add('Radio', () => {
+    const [selected, setSelected] = React.useState(choices[0]);
+    return (
+      <Radio name={'newTab'} choices={choices} selected={selected} onSelect={choice => setSelected(choice)}/>
+    );
+  });
